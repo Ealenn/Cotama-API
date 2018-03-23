2333c4ef6637f8a8e208b9eb47cc43fde9aab7e7
Marchandise Rudy <r.marchandise@sfr.fr>
2018-03-23 10:29:57 +0100

    Update middleware CORS
    https://github.com/barryvdh/laravel-cors

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Route;
 use Laravel\Passport\Passport;
