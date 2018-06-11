9cd23e3336e11c9898b2acca594c8df968047bf9
Marchandise Rudy <r.marchandise@sfr.fr>
2018-06-11 15:55:06 +0200

    DNS Config .net

--- config/mail.php
+++ config/mail.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'driver' => env('MAIL_DRIVER', 'smtp'),
+    'driver' => env('MAIL_DRIVER', 'mailgun'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'l.david174@laposte.net'),
-        'name' => env('MAIL_FROM_NAME', 'COTAMA TEAM'),
+        'address' => env('MAIL_FROM_ADDRESS', 'noreply@cotama.net'),
+        'name' => env('MAIL_FROM_NAME', 'COTAMA'),
     ],
 
     /*
